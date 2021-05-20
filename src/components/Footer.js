1ddff4f8d65076f8fca4bdbd08bdc850709478d8
Kancherla Kishorereddy <49707819+kancherlakishorereddy@users.noreply.github.com>
2021-05-20 17:24:50 +0530

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="jumbotron jumbotron-fluid bg-primary text-white py-4 my-0">
             <div className="container text-center">
-                -Made by <a href="https:/kancherlakishorereddy.github.io" target="null"><strong>K.Kishorereddy</strong>
+                -Made by <a href="https://kancherlakishorereddy.github.io" target="_null"><strong>K.Kishorereddy</strong>
             </a></div>
         </footer>
     )
